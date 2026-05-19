a24dc60e9b2e02ff1529ec82e8cb663dadd86416
jee-mj <nobody@github.com.au>
2026-05-19 12:11:15 +1000

    refactor: modernize ultimate-autopair.nix plugin installation

--- config/utils/ultimate-autopair.nix
+++ config/utils/ultimate-autopair.nix
@@ -10,17 +10,8 @@
   };
   config = lib.mkIf config.ultimate-autopair.enable {
 
-    extraPlugins = with pkgs.vimUtils; [
-      (buildVimPlugin {
-        pname = "ultimate-autopair.nvim";
-        version = "2024-02-05";
-        src = pkgs.fetchFromGitHub {
-          owner = "altermo";
-          repo = "ultimate-autopair.nvim";
-          rev = "25c13e0ce167db0255456cac10158b27d2be30c0";
-          sha256 = "0bsxfj6g8fii9nn92vl15hdhafx3fikgiz4srr7y10pxz01c5s4c";
-        };
-      })
+    extraPlugins = with pkgs.vimPlugins; [
+      ultimate-autopair-nvim
     ];
     extraConfigLua = ''
       require('ultimate-autopair').setup()
