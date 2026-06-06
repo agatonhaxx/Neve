f94dc30483d00f45240a644aadf82b729135b2cc
redyf <mateusalvespereira7@gmail.com>
2026-06-06 00:58:18 -0300

    remove(comment): unnecessary comment

--- config/utils/plenary.nix
+++ config/utils/plenary.nix
@@ -9,8 +9,6 @@
     plenary.enable = lib.mkEnableOption "Enable plenary module";
   };
   config = lib.mkIf config.plenary.enable {
-    # NOTE: plenary-nvim is a transitive dependency of telescope and many other
-    # plugins. This explicit install is redundant but harmless.
     extraPlugins = with pkgs.vimPlugins; [
       plenary-nvim
     ];
